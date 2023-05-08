07c175ab265cba1fc8bc1b2882cfd2bd75e6acd6
Kushzz18 <kushalpathak18@gmail.com>
2023-05-08 22:02:01 +0545

    New

---
+++ Shooter/src/GamePanel.java
@@ -0,0 +1,51 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable, KeyListener {
+    private static final long serialVersionUID = 10L;
+    private Color bgColor = new Color(50, 100, 100);
+    public static int WIDTH;
+    public static int HEIGHT;
+    private int FPS;
+    private boolean running;
+    public Graphics2D g;
+    public BufferedImage image;
+    private Thread thread;
+    public static Player player;
+    public static ArrayList<Enemy> enemies;
+    public static ArrayList<Bullet> bullets;
+    public static ArrayList<Explosion> explosions;
+    public static ArrayList<PowerUp> powerups;
+    private Laser laser;
+    private boolean laserTaken;
+    private long waveStartTimer;
+    private long waveStartTimerDiff;
+    private int waveDelay;
+    private int waveNumber;
+    private boolean waveStart;
+    private long slowStartTimer;
+    private int slowLength;
+    private long slowElapsed;
+    public GamePanel() {
+        WIDTH = 600;
+        HEIGHT = 450;
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.FPS = 60;
+        this.setFocusable(true);
+        this.requestFocus();
+        this.waveStartTimer = 0L;
+        this.waveStartTimerDiff = 0L;
+        this.waveDelay = 2000;
+        this.slowLength = 15000;
+    }
---
+++ Shooter/src/Shooter_Game.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Shooter_Game {
+    public Shooter_Game() {
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("First Game");
+        window.setDefaultCloseOperation(3);
+        window.pack();
+        window.setContentPane(new GamePanel());
+        window.setResizable(false);
+        window.setVisible(true);
+        window.pack();
+    }
+}
