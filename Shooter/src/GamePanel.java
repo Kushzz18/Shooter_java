40efcab87356876006f5211726dbf44d01895e92
Kushzz18 <kushalpathak18@gmail.com>
2023-05-09 08:29:18 +0545

    update

--- Shooter/src/GamePanel.java
+++ Shooter/src/GamePanel.java
@@ -22,12 +22,12 @@ public class GamePanel extends JPanel implements Runnable,KeyListener{
     public Graphics2D g;
     public BufferedImage image;
     private Thread thread;
-    //public static Player player;
-    //public static ArrayList<Enemy> enemies;
+    public static Player player;
+    public static ArrayList<Enemy> enemies;
     public static ArrayList<Bullet> bullets;
-    //public static ArrayList<Explosion> explosions;
-    //public static ArrayList<PowerUp> powerups;
-   // private Laser laser;
+    public static ArrayList<Explosion> explosions;
+    public static ArrayList<PowerUp> powerups;
+    private Laser laser;
     private boolean laserTaken;
     private long waveStartTimer;
     private long waveStartTimerDiff;
@@ -59,6 +59,33 @@ public class GamePanel extends JPanel implements Runnable,KeyListener{
             this.thread.start();
         }
         this.addKeyListener(this);
+    }
+    public void run(){
+        this.running = true;
+        this.image= new BufferedImage(WIDTH,HEIGHT,1);
+        this.g = (Graphics2D) this.image.getGraphics();
+        //player = new Player();
+        enemies = new ArrayList();
+        bullets = new ArrayList();
+        powerups = new ArrayList();
+        explosions = new ArrayList();
+        this.g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        this.g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        long targetTime = (long)(1000 / this.FPS);
+        while(this.running){
+            long startTime = System.nanoTime();
+            this.gameUpdate();
+            //this.gameRender();
+            //this.gamedraw();
+            long URDTimeMillis = (System.nanoTime() - startTime) / 1000000L;
+            long waitTime = targetTime - URDTimeMillis;
+
+            try {
+                Thread.sleep(waitTime);
+            } catch (Exception var10) {
+            }
+        }
+        }
     }
 
     @Override
